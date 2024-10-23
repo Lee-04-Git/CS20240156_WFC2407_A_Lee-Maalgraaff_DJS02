71895914389d95472db6c584ee010ae7454dc0e7
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-10-23 22:48:36 +0200

    added validation to check for empty dividend and divider inputs

--- scripts.js
+++ scripts.js
@@ -7,7 +7,14 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
 
+    // Scenario 1 - Dividing numbers result in a decimal number
     const wholeNumberResult = Math.trunc(dividend / divider);
-  
     result.innerText = wholeNumberResult;
+
+     // Scenario 2 - Validation when values are missing
+    if (!dividend || !divider) {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+      return;
+    }
+
 });
