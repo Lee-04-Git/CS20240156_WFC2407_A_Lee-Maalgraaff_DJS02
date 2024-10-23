4a665814ddb21679cb2af095565832606b82eb48
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-10-23 22:40:35 +0200

    used math.trunc to show whole number result from division

--- scripts.js
+++ scripts.js
@@ -6,4 +6,8 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+    const wholeNumberResult = Math.trunc(dividend / divider);
+  
+    result.innerText = wholeNumberResult;
 });
