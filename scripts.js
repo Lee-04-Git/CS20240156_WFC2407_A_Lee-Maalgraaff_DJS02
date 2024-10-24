296ea5290dfa36d5ee277e039f35600ab683f4a8
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-10-24 12:59:43 +0200

    indented if statement

--- scripts.js
+++ scripts.js
@@ -18,13 +18,13 @@ form.addEventListener("submit", (event) => {
     }
 
     // Scenario 3 - Invalid division (division by zero)
-  if (divider === "0") {
-    result.innerText = "Division not performed. Invalid number provided. Try again.";
-    
-    console.error(new Error("Division by zero"));
-    
-    return;
-  }
+    if (divider === "0") {
+      result.innerText = "Division not performed. Invalid number provided. Try again.";
+      
+      console.error(new Error("Division by zero"));
+      
+      return;
+    }
 
 
 
